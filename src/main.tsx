850dcecd22706be46f5be676e730ba81b40bc696
Aman Nasir <aman.nasir231@gmail.com>
2026-03-23 17:08:52 +0500

    Fix Toast Message

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import { Toaster } from './components/ui/toaster.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ChakraProvider value={defaultSystem}>
+        <Toaster /> 
       <App />
     </ChakraProvider>
   </StrictMode>,
